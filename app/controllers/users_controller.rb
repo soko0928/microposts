ea0f8e8ea24e027c6279223857b22b881f3b4d71
soko <sfujino@netprotections.co.jp>
2016-06-20 02:15:20 +0000

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
-def show # 追加
+  def show # 追加
    @user = User.find(params[:id])
-end
+  end
   
   def new
     @user = User.new
   end
 
-def create
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
@@ -16,10 +16,26 @@ def create
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to @user , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :local, :password,
                                  :password_confirmation)
   end
+end
